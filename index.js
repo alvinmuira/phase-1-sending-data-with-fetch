1441bf4762b4fcda7dae8a98c6552713bfbef0da
Alvin Muira <alvinmuira@gmail.com>
2025-07-07 00:57:02 +0300

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+
+function submitData (name, email) {
+    const sentData = {
+        name: `${name}`,
+        email: `${email}`
+    }
+
+    const theFetch =fetch(`http://localhost:3000/users`, {
+        method: `POST`,
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(sentData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const p = document.createElement("p");
+        p.textContent = data.id;
+        document.body.appendChild(p);
+    })
+    .catch(error => {
+      const p = document.createElement("p");
+      p.textContent = error.message;
+      document.body.appendChild(p);
+    });
+return theFetch;
+}
+
